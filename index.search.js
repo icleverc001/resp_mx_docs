06321ad4c96fac267222961ad4a08b92fe5b3009
Yusuke Tagawa <whis695@yahoo.co.jp>
2024-05-08 10:57:47 +0900

    add page

---
+++ index.search.js
@@ -0,0 +1,50 @@
+var relearn_search_index = [
+  {
+    "breadcrumb": "RESP-MX操作・計算マニュアル",
+    "content": "\r",
+    "description": "",
+    "tags": null,
+    "title": "操作編",
+    "uri": "/operations/index.html"
+  },
+  {
+    "breadcrumb": "RESP-MX操作・計算マニュアル",
+    "content": "\r",
+    "description": "",
+    "tags": null,
+    "title": "計算編",
+    "uri": "/calculations/index.html"
+  },
+  {
+    "breadcrumb": "",
+    "content": "\r操作編\r計算編\rテスト\r",
+    "description": "",
+    "tags": null,
+    "title": "RESP-MX操作・計算マニュアル",
+    "uri": "/index.html"
+  },
+  {
+    "breadcrumb": "RESP-MX操作・計算マニュアル",
+    "content": "This is a new chapter.\nrg,lpea^^^ ghrghrea ",
+    "description": "",
+    "tags": null,
+    "title": "テスト",
+    "uri": "/basics/index.html"
+  },
+  {
+    "breadcrumb": "RESP-MX操作・計算マニュアル",
+    "content": "",
+    "description": "",
+    "tags": null,
+    "title": "カテゴリー",
+    "uri": "/categories/index.html"
+  },
+  {
+    "breadcrumb": "RESP-MX操作・計算マニュアル",
+    "content": "",
+    "description": "",
+    "tags": null,
+    "title": "タグ",
+    "uri": "/tags/index.html"
+  }
+]
---
+++ pir/index.search.js
@@ -0,0 +1,26 @@
+var relearn_search_index = [
+  {
+    "breadcrumb": "",
+    "content": "",
+    "description": "",
+    "tags": null,
+    "title": "Cap'n Hugo Relearrrn Theme",
+    "uri": "/pir/index.html"
+  },
+  {
+    "breadcrumb": "Cap'n Hugo Relearrrn Theme",
+    "content": "",
+    "description": "",
+    "tags": null,
+    "title": "Categorrries",
+    "uri": "/pir/categories/index.html"
+  },
+  {
+    "breadcrumb": "Cap'n Hugo Relearrrn Theme",
+    "content": "",
+    "description": "",
+    "tags": null,
+    "title": "Taks",
+    "uri": "/pir/tags/index.html"
+  }
+]
